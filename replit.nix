3fca5eaab5879b51e566bfa999fdd2c88694e9fb
baadakk <anandbukan143np@gmail.com>
2024-07-11 11:43:39 +0000

    x

---
+++ replit.nix
@@ -0,0 +1,18 @@
+
+
+{pkgs}: {
+  deps = [
+    pkgs.imagemagick_light
+    
+    
+  ];
+}
+
+
+# pkgs.imagemagick_light
+# pkgs.python310
+# pkgs.unixODBC
+# pkgs.unixODBC.dev
+# pkgs.msodbcsql17
+# pkgs.python310Packages.sqlalchemy
+# pkgs.python310Packages.pyodbc

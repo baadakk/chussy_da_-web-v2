ead67fcf288117f7859b598622507862acdf62a6
baadakk <anandbukan143np@gmail.com>
2024-07-12 06:28:29 +0000

    adddff

--- database.py
+++ database.py
@@ -13,3 +13,11 @@ def load_jobs_from_db():
             jobs.append(row._asdict())
         return jobs  # Return all columns from the database
 
+def load_job_from_db(id):
+    with engine.connect() as conn:
+     result = conn.execute(text("SELECT * FROM Jobs WHERE id = :val"), {'val': id})
+     rows = result.all() 
+     if len(rows) == 0:
+         return None
+     else:
+      return dict(rows[0])

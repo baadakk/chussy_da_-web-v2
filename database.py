cfdba2273ff0e740206ff41ecf3bfaaf7d44d918
baadakk <anandbukan143np@gmail.com>
2024-07-14 08:09:51 +0000

    Added requirments

--- database.py
+++ database.py
@@ -1,23 +1,14 @@
-from sqlalchemy import create_engine,text
+
+from sqlalchemy import create_engine, text
 import os
+
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 engine = create_engine(db_connection_string)
 
-
-
 def load_jobs_from_db():
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT * FROM Jobs"))
-        jobs = []
-        for row in result.all():
-            jobs.append(row._asdict())
-        return jobs  # Return all columns from the database
-
-def load_job_from_db(id):
-    with engine.connect() as conn:
-     result = conn.execute(text("SELECT * FROM Jobs WHERE id = :val"), {'val': id})
-     rows = result.all() 
-     if len(rows) == 0:
-         return None
-     else:
-      return dict(rows[0])
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from Jobs"))
+    jobs = []
+    for row in result.mappings():
+      jobs.append(dict(row))
+    return jobs

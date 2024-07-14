70e0b8744780993bc02bfbef0b330f424667ea92
baadakk <anandbukan143np@gmail.com>
2024-07-14 08:48:51 +0000

    aa

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
  
 from flask import Flask, jsonify, render_template
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -13,6 +13,14 @@ def hello_jovian():
 def list_jobs(): 
     jobs = load_jobs_from_db()
     return jsonify(jobs)
+
+@app.route("/job/<id>")
+def show_job(id):
+    job = load_job_from_db(id)  # Fetch the job based on the ID
+    if job:
+        return jsonify(job)
+    else:
+        return jsonify({'error': 'Job not found'}), 404
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 
 from sqlalchemy import create_engine, text
-import os
+import os 
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 engine = create_engine(db_connection_string)
@@ -12,3 +12,12 @@ def load_jobs_from_db():
     for row in result.mappings():
       jobs.append(dict(row))
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from Jobs where id = :val"), {"val": id})
+    rows = result.mappings().all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])  
